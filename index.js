0c058f65a5c2d97a3fb58c7f23a708d6f43c2d0f
Tabioka <donovanbrady102@gmail.com>
2025-02-05 03:19:40 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
+const keepAlive = require("./keep_alive");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.commands = new Collection();
+
+// Load command files
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+// Load event files
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  client.on(event.name, (...args) => event.execute(...args, client));
+}
+
+client.login(process.env.TOKEN);
